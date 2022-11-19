d7f266e2f9e3003e9b64cf59a9a7f4ccf282da9f
spectr-e <devs.josia@gmail.com>
2022-11-19 13:39:00 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+function writeCards(names){
+    let messages = []
+    for(let i in names) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return messages;
+}
+function countDown(number){
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
